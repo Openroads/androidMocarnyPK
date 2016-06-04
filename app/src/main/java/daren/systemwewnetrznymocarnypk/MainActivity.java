8ade0b9c05e7a0e092661fb009f4ca1117656c2a
daren <daren22@o2.pl>
2016-06-04 11:31:00 +0200

    Initial commit

---
+++ app/src/main/java/daren/systemwewnetrznymocarnypk/MainActivity.java
@@ -0,0 +1,41 @@
+package daren.systemwewnetrznymocarnypk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadContent();
+
+    }
+    private void loadContent()
+    {
+        Button about_button = (Button) findViewById(R.id.button2);
+        Button log_in_button = (Button) findViewById(R.id.button);
+        assert about_button != null;
+        about_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,About.class);
+                startActivity(intent);
+            }
+        });
+
+        assert log_in_button != null;
+        log_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
