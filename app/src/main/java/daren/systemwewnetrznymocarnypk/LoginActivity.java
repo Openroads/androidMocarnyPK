d53a481eb0b47b04d1ce81ec30817774a80c39ef
daren <daren22@o2.pl>
2016-06-04 15:23:52 +0200

    logowanie i kalendarz same layouty

--- app/src/main/java/daren/systemwewnetrznymocarnypk/LoginActivity.java
+++ app/src/main/java/daren/systemwewnetrznymocarnypk/LoginActivity.java
@@ -3,6 +3,7 @@ package daren.systemwewnetrznymocarnypk;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -187,12 +188,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent login = new Intent(LoginActivity.this,NavigationActivity.class);
+            startActivity(login);
         }
     }
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return true;
     }
 
     private boolean isPasswordValid(String password) {
@@ -333,6 +336,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
