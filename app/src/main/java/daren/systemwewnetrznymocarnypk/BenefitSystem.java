989d962dfcc980493bc13857c3a32332d2e2735a
daren <daren22@o2.pl>
2016-06-05 02:32:48 +0200

    czyste ProjectActivity i Benefity

---
+++ app/src/main/java/daren/systemwewnetrznymocarnypk/BenefitSystem.java
@@ -0,0 +1,13 @@
+package daren.systemwewnetrznymocarnypk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BenefitSystem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_benefit_system);
+    }
+}
